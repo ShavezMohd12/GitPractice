873925e8192d2b8fe882fd70ad9ad13aa2ea5e21
ShavezMohd12 <9554412379s@gmail.com>
2026-07-22 18:56:33 +0530

    added conditionalWorkflow

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('should fail because text does not exist', () => {
+  render(<App />);
+  expect(screen.getByText('Welcome to React')).toBeInTheDocument();
+});
